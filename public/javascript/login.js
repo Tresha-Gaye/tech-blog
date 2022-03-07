9b22690aca4de798c688d6f41a416becbccfee2a
Twigster <treshagaye@gmail.com>
2022-03-07 00:53:20 -0500

    attempted onclick bug fix for login-signup forms-

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,34 +1,3 @@
-// submit form handler
-
-async function signupFormHandler(event) {
-    event.preventDefault();
-  
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-  
-    if (username && email && password) {
-      const response = await fetch('/api/users', {
-        method: 'post',
-        body: JSON.stringify({
-          username,
-          email,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
-  
-      // check the response status
-      if (response.ok) {
-        console.log('success');
-      } else {
-        alert(response.statusText);
-      }
-    }
-}
-
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
-
 // login form handler
 
 async function loginFormHandler(event) {
@@ -59,4 +28,36 @@ async function loginFormHandler(event) {
     }
 }
   
-document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+document.querySelector('.login-btn').addEventListener('submit', loginFormHandler);
+
+// submit form handler
+
+async function signupFormHandler(event) {
+    event.preventDefault();
+  
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+  
+    if (username && email && password) {
+      const response = await fetch('/api/users', {
+        method: 'post',
+        body: JSON.stringify({
+          username,
+          email,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+      // check the response status
+      if (response.ok) {
+        console.log('success');
+      } else {
+        alert(response.statusText);
+      }
+    }
+}
+
+document.querySelector('.signup-btn').addEventListener('submit', signupFormHandler);
+
